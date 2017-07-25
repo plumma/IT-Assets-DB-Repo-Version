48799561f7c887ab7bc116723af16c75522c0ea3
Matt Plumley <matt.plumley@gmail.com>
2017-07-25 17:08:29 +0100

    RemoveNAV

--- ITAssetsDatabase.Web/App_Start/RouteConfig.cs
+++ ITAssetsDatabase.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ITAssetsDatabase.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Assets", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                  //namespaces: new[] { "ITAssetsDatabase.Web.Controllers" }
             );
         }
